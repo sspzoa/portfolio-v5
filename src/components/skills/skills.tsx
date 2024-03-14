c77d2d5806af18889f8befae9a8845ffcd5db1e7
Seungpyo Suh <sspyorea@gmail.com>
2024-03-15 08:30:24 +0900

    update: skills

--- src/components/skills/skills.tsx
+++ src/components/skills/skills.tsx
@@ -6,11 +6,11 @@ export default function Skills() {
         <div id='skills' className={styles.container}>
             <div className={styles.content}>
                 <Image src={'/images/skills/flutter.svg'} alt={'flutter'} width={250} height={100} style={{objectFit: "contain"}} />
-                <Image src={'/images/skills/android.svg'} alt={'flutter'} width={250} height={100} style={{objectFit: "contain"}} />
-                <Image src={'/images/skills/next.svg'} alt={'flutter'} width={250} height={100} style={{objectFit: "contain"}} />
-                <Image src={'/images/skills/firebase.svg'} alt={'flutter'} width={250} height={100} style={{objectFit: "contain"}} />
+                <Image src={'/images/skills/android.svg'} alt={'android'} width={250} height={100} style={{objectFit: "contain"}} />
+                <Image src={'/images/skills/reactnative.svg'} alt={'flutter'} width={250} height={100} style={{objectFit: "contain"}} />
+                <Image src={'/images/skills/next.svg'} alt={'next'} width={250} height={100} style={{objectFit: "contain"}} />
+                <Image src={'/images/skills/nest.svg'} alt={'flutter'} width={250} height={100} style={{objectFit: "contain"}} />
                 <Image src={'/images/skills/spring.svg'} alt={'spring'} width={250} height={100} style={{objectFit: "contain"}} />
-                <Image src={'/images/skills/node.svg'} alt={'flutter'} width={250} height={100} style={{objectFit: "contain"}} />
             </div>
         </div>
     );
